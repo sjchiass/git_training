d25b5fcfc97172d917c23835bef8465a1ac370eb
Sebastien Chiasson <sebastien.chiasson@canada.ca>
2020-12-02 11:23:04 -0500

    Make merge_b branch

--- iris.R
+++ iris.R
@@ -1,17 +1,11 @@
-# Pivot iris dataset
-iris_pivot <- lapply(X = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
-                     FUN = function(x) {
-                       data.frame(Species = iris$Species,
-                                  Measure = x,
-                                  Value = iris[[x]])
-                       })
-iris_pivot <- do.call(rbind, iris_pivot)
+library(tidyverse)
 
-# Calculate means of each Species and Measure combination
-means <- aggregate(x = iris_pivot$Value,
-          by = list(Species = iris_pivot$Species,
-                    Measure = iris_pivot$Measure),
-          FUN = mean)
+# Pivot and summarize
+iris_pivot <- iris %>%
+  pivot_longer(-Species, "Measure") %>%
+  group_by(Species, Measure) %>%
+  summarise_all(mean) %>%
+  ungroup()
 
 # Show results
 print(means)
